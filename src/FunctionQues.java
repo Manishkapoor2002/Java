c5ae7f75a55bc00cb55651e8860b13c746e93860
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-08-24 01:19:34 +0530

    Number is Even or not.

--- src/FunctionQues.java
+++ src/FunctionQues.java
@@ -2,19 +2,31 @@ import java.util.Scanner;
 
 public class FunctionQues {
 
-    public static int AVG(int num1,int num2,int num3){
-        return (num1+num2+num3)/3;
+//    Average of Three Numbers:
+//    public static int AVG(int num1,int num2,int num3){
+//        return (num1+num2+num3)/3;
+//    }
+
+//    Even or not
+    public static boolean isEven(int num){
+        if(num%2==0){
+            return true;
+        }
+        return false;
     }
     public static void main(String args[]){
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Enter First Number: ");
-        int a = scn.nextInt();
-        System.out.print("Enter Second Number: ");
-        int b = scn.nextInt();
-        System.out.print("Enter Third Number: ");
-        int c = scn.nextInt();
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter First Number: ");
+//        int a = scn.nextInt();
+//        System.out.print("Enter Second Number: ");
+//        int b = scn.nextInt();
+//        System.out.print("Enter Third Number: ");
+//        int c = scn.nextInt();
+//
+//        float ans = AVG(a,b,c);
+//        System.out.println(ans);
 
-        float ans = AVG(a,b,c);
-        System.out.println(ans);
+        System.out.println(isEven(55));
+
     }
 }
