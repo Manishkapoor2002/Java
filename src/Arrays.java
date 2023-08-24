16b54183d57e74c37af4f5473f2da65abae5ce31
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-08-25 02:00:21 +0530

    Finding Greatest and Smallest Val using Linear search.

--- src/Arrays.java
+++ src/Arrays.java
@@ -11,15 +11,39 @@ public class Arrays {
 //    }
 
 //    Linear Search:
+//-> Finding a number
+//    public static int FindNum(int Num[], int key) {
+//        for (int i = 0; i <= Num.length - 1; i++) {
+//            if (Num[i] == key) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
 
-    public static int FindNum(int Num[], int key) {
-        for (int i = 0; i <= Num.length - 1; i++) {
-            if (Num[i] == key) {
-                return i;
+//    -> Finding Largest value in Array:
+    public static int LargestVal(int[] Rank){
+        int maxRank = Integer.MIN_VALUE;//- infinity
+
+        for (int i = 0;i<= Rank.length-1;i++){
+            if(Rank[i]>maxRank){
+                maxRank = Rank[i];
             }
         }
-        return -1;
+        return maxRank;
     }
+
+    public static int SmallestVal(int[] Rank){
+        int minRank = Integer.MAX_VALUE;//- infinity
+
+        for (int i = 0;i<= Rank.length-1;i++){
+            if(Rank[i]<minRank){
+                minRank = Rank[i];
+            }
+        }
+        return minRank;
+    }
+
 
     public static void main(String args[]){
 //        creating Array:
@@ -43,14 +67,20 @@ public class Arrays {
 //        System.out.println(marks[2]);
 
 //        Linear Search:
-        int key = 5;
-        int Number[] = {1,2,3,4,5,6,7,8,9,10};
-        int index = FindNum(Number,key);
+//        int key = 5;
+//        int Number[] = {1,2,3,4,5,6,7,8,9,10};
+//        int index = FindNum(Number,key);
+//
+//        if(index == -1){
+//            System.out.println("Key not Found");
+//        }else{
+//            System.out.println("key found at " +index);
+//        }
 
-        if(index == -1){
-            System.out.println("Key not Found");
-        }else{
-            System.out.println("key found at " +index);
-        }
+        int[] Rank = {99,54,105,64,78,254,11};
+
+        System.out.println("Maximum Rank among all :" +LargestVal(Rank));
+        System.out.println("Smallest Rank among all :" +SmallestVal(Rank));
+
         }
     }
