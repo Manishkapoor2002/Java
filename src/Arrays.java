c17f8b9cd2629bb8330c1db6eb21e437ba9998f8
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-08-25 02:50:50 +0530

    Binary search!

--- src/Arrays.java
+++ src/Arrays.java
@@ -22,30 +22,54 @@ public class Arrays {
 //    }
 
 //    -> Finding Largest value in Array:
-    public static int LargestVal(int[] Rank){
-        int maxRank = Integer.MIN_VALUE;//- infinity
+//    public static int LargestVal(int[] Rank){
+//        int maxRank = Integer.MIN_VALUE;//- infinity
+//
+//        for (int i = 0;i<= Rank.length-1;i++){
+//            if(Rank[i]>maxRank){
+//                maxRank = Rank[i];
+//            }
+//        }
+//        return maxRank;
+//    }
+//
+//    public static int SmallestVal(int[] Rank){
+//        int minRank = Integer.MAX_VALUE;//- infinity
+//
+//        for (int i = 0;i<= Rank.length-1;i++){
+//            if(Rank[i]<minRank){
+//                minRank = Rank[i];
+//            }
+//        }
+//        return minRank;
+//    }
 
-        for (int i = 0;i<= Rank.length-1;i++){
-            if(Rank[i]>maxRank){
-                maxRank = Rank[i];
+
+//    Binary Search:
+
+    public static int binarySearch(int number[], int key) {
+        int start = 0, end = number.length - 1;
+
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            System.out.println(mid);
+
+            if (number[mid] == key) {
+                return mid;
             }
+            if (number[mid] < key) {
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+
         }
-        return maxRank;
+        return -1;
     }
 
-    public static int SmallestVal(int[] Rank){
-        int minRank = Integer.MAX_VALUE;//- infinity
 
-        for (int i = 0;i<= Rank.length-1;i++){
-            if(Rank[i]<minRank){
-                minRank = Rank[i];
-            }
-        }
-        return minRank;
-    }
-
-
-    public static void main(String args[]){
+    public static void main(String args[]) {
 //        creating Array:
 //        DataType ArrayName[] = new DataType[Size of Array]
 //        int numbers[] = new int[5];
@@ -77,10 +101,17 @@ public class Arrays {
 //            System.out.println("key found at " +index);
 //        }
 
-        int[] Rank = {99,54,105,64,78,254,11};
+//        Finding Maximum and Smallest Value of an Array
+//        int[] Rank = {99,54,105,64,78,254,11};
+//
+//        System.out.println("Maximum Rank among all :" +LargestVal(Rank));
+//        System.out.println("Smallest Rank among all :" +SmallestVal(Rank));
 
-        System.out.println("Maximum Rank among all :" +LargestVal(Rank));
-        System.out.println("Smallest Rank among all :" +SmallestVal(Rank));
+//        Binary Search:
+        int number[] = {2,4,6,8,10,12,14,16,18,20};
+        int key = 12;
+        System.out.println("Key found at" +binarySearch(number, key));
 
-        }
+
     }
+}
