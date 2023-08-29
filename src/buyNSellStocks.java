0659bcf30d2c082fbbdf964fee3bc502543b57aa
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-08-30 03:05:46 +0530

    OPTIMISED Buying and Selling of stock at maximum profit problem.

--- src/buyNSellStocks.java
+++ src/buyNSellStocks.java
@@ -3,22 +3,32 @@ public class buyNSellStocks {
     public static int maxProfit(int[] prices){
         int n = prices.length;
 
+//        int maxprofit = 0;
+//        int currentprofit = 0;
+//        for(int i = 0;i<n;i++){
+//            for (int j = i+1;j<n;j++){
+//                currentprofit = (prices[j] - prices[i]);
+//                maxprofit = Math.max(maxprofit,currentprofit);
+//            }
+//        }
+//        return maxprofit;
+
+        int buyPrice = Integer.MAX_VALUE;
         int maxprofit = 0;
-        int currentprofit = 0;
-        for(int i = 0;i<n;i++){
-            for (int j = i+1;j<n;j++){
-                currentprofit = (prices[j] - prices[i]);
-                maxprofit = Math.max(maxprofit,currentprofit);
-                if (maxprofit<0){
-                    maxprofit = 0;
-                }
+
+        for (int i = 0;i<n;i++){
+            if (buyPrice<prices[i]){
+                int currentprofit = prices[i] - buyPrice;
+                maxprofit = Math.max(currentprofit,maxprofit);
+            }else{
+                buyPrice = prices[i];
             }
         }
-        return maxprofit;
 
+        return maxprofit;
     }
     public static void main(String[] args) {
-        int[] prices = {5,4,3,2,1,0};
+        int[] prices = {7,1,5,3,6,4};
        int profit = maxProfit(prices);
         System.out.println(profit);
     }
