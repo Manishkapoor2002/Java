2ca988091f72a744093ae704492742d33c52c1c4
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-09 08:54:05 +0530

    Finding Min Value and Max Value in 2D Array.

--- src/Array_2D/introduction.java
+++ src/Array_2D/introduction.java
@@ -1,20 +1,37 @@
 package Array_2D;
 
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class introduction {
 
-    public static boolean Search(int[][] arr,int k){
+    public static int[] minNmax(int[][] arr){
+        int[] ans = new int[2];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
         for (int i =0;i< arr.length;i++){
             for (int j = 0;j< arr[0].length;j++){
-               if (arr[i][j]==k){
-                   return true;
-               }
+                min = Math.min(min,arr[i][j]);
+                max = Math.max(max,arr[i][j]);
             }
         }
-        return false;
+        ans[0] = min;
+        ans[1] = max;
+        return ans;
     }
+
+//    public static boolean Search(int[][] arr,int k){
+//
+//        for (int i =0;i< arr.length;i++){
+//            for (int j = 0;j< arr[0].length;j++){
+//               if (arr[i][j]==k){
+//                   return true;
+//               }
+//            }
+//        }
+//        return false;
+//    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter the value of n : ");
@@ -27,10 +44,16 @@ public class introduction {
                 arr2D[i][j] = scn.nextInt();
             }
         }
-        System.out.print("Enter the value of key : ");
-        int key = scn.nextInt();
-        boolean ans = Search(arr2D,key);
-        System.out.println(ans);
+//        System.out.print("Enter the value of key : ");
+//        int key = scn.nextInt();
+//        boolean ans = Search(arr2D,key);
+//        System.out.println(ans);
 
+       int[] ans = minNmax(arr2D);
+
+        System.out.println("Minimum and Maximum Values are : ");
+        for(int i =0;i<ans.length;i++){
+            System.out.println(ans[i]);
+        }
     }
 }
