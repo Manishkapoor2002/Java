83acec138865e39edf344ce521c27051fcfc80ec
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-16 11:04:36 +0530

    compression operator

--- src/Strings/Introduction.java
+++ src/Strings/Introduction.java
@@ -4,34 +4,51 @@ import java.util.Scanner;
 
 public class Introduction {
     public static void main(String[] args) {
-//        Strings are Immutable(not changeable)
+////        Strings are Immutable(not changeable)
+//
+//        char[] ch = {'a','b','c'};
+//        String string = "Manish kapoor";
+//
+//        for (int i = 0;i<ch.length;i++){
+//            System.out.println(ch[i]);
+//        }
+//        System.out.println(string);
+//
+////Input in String and length() Function :
+//        System.out.print("Your Name is : ");
+//        Scanner scn = new Scanner(System.in);
+//        String name = scn.nextLine();
+//        System.out.println("Your Name is : "+name +" the length of the name is : " +name.length());
+//
+////        concatenation:
+//        String firstName = "Manish ";
+//        String lastName = "Kapoor";
+//        String fullName = firstName + lastName;
+//
+//        System.out.println(fullName);
+//
+////        charAt function
+//
+//        for (int i =0;i<fullName.length();i++){
+//            System.out.print(fullName.charAt(i) + " , ");
+//        }
 
-        char[] ch = {'a','b','c'};
-        String string = "Manish kapoor";
+//        Compression operator for string
+        String s1 = "Manish";
+        String s3 = new String("Manish");
 
-        for (int i = 0;i<ch.length;i++){
-            System.out.println(ch[i]);
+
+//        by using compression operator
+        if (s1.equals(s3)){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
         }
-        System.out.println(string);
-
-//Input in String and length() Function :
-        System.out.print("Your Name is : ");
-        Scanner scn = new Scanner(System.in);
-        String name = scn.nextLine();
-        System.out.println("Your Name is : "+name +" the length of the name is : " +name.length());
-
-//        concatenation:
-        String firstName = "Manish ";
-        String lastName = "Kapoor";
-        String fullName = firstName + lastName;
-
-        System.out.println(fullName);
-
-//        charAt function
-
-        for (int i =0;i<fullName.length();i++){
-            System.out.print(fullName.charAt(i) + " , ");
+//        by using ==
+        if (s1 == s3){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
         }
-
     }
 }
